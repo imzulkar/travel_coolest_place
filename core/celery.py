39153eb3dae5580712c9c5dbc0ec0f7f7047e85d
imzulkar <gmzulkar@gmail.com>
2023-10-07 02:00:46 +0600

    FEAT: docker file added for redis and celery and assign peroic task for weather update
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

--- core/celery.py
+++ core/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from celery.schedules import crontab
+from datetime import datetime, timedelta
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("core")
@@ -9,9 +10,15 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 
-app.conf.beat_schedule = {  # scheduler configuration
-    "test_task": {
-        "task": "weather.tasks.test",
-        "schedule": 2,
+# set today
+today = datetime.today().date()
+
+# scheduler configuration
+app.conf.beat_schedule = {
+    "weather_info": {
+        "task": "weather.tasks.get_weather_info",
+        "schedule": crontab(hour=1, minute=00),
+        # "schedule": 10,
+        "args": {today, today + timedelta(days=6)},
     }
 }
--- weather/tasks.py
+++ weather/tasks.py
@@ -1,9 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
+from datetime import datetime, timedelta
+from weather.weather import get_districts_weather
 
 app = Celery()
 
 
+# @app.task
+# def test():
+#     print("runnning test task")
+
+
 @app.task
-def test():
-    print("runnning test task")
+def get_weather_info(start_date=None, end_date=None):
+    if start_date is None:
+        today = datetime.now().date()
+        start_date = today
+        end_date = today + timedelta(days=7)
+    print(start_date, end_date)
+    get_districts_weather(start_date, end_date)
